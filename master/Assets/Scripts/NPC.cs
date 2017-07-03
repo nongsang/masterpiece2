798b6c4f33aabf8fdf84799ceea7f83f7bb8678c
nongsang <shdtkd37@gmail.com>
2017-07-03 18:06:24 +0900

    7월 3일 오피스 지진 이벤트

--- master/Assets/Scripts/NPC.cs
+++ master/Assets/Scripts/NPC.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NPC : MonoBehaviour {
+
+    public GameObject destination;
 
 	// Use this for initialization
 	void Start () {
